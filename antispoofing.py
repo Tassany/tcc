23cd7fdff409ca163409364daa3b3585691af895
Tassany <tassanyonofre@gmail.com>
2024-07-09 22:42:43 -0300

    Novas fotos falsas no papel fotográfico

---
+++ antispoofing.py
@@ -0,0 +1,69 @@
+# import module
+from datetime import datetime
+import time
+import cv2
+import numpy as np
+from ndvi.ndvi_processing import ndvi_calculate
+
+max_mean = 0
+min_mean = 255
+# Saved frame count
+count = 0
+# allow the camera to warmup
+time.sleep(0.1)
+
+def is_attack(image):
+    global max_mean, min_mean
+    attack_threshold = 0
+    # Split the channels
+    _, _, r = cv2.split(image)
+    
+    ndvi = ndvi_calculate(image)
+
+    x_start = int(r.shape[0]/2 - r.shape[0]/6)
+    x_end = int(r.shape[0]/2 + r.shape[0]/6)
+    y_start = int(r.shape[1]/2 - r.shape[1]/6)
+    y_end = int(r.shape[1]/2 + r.shape[1]/6)
+
+    mean = np.mean(ndvi[x_start:x_end, y_start:y_end])
+
+    if mean > max_mean:
+        max_mean = mean
+    if mean < min_mean:
+        min_mean = mean
+
+    print(f'mean: {mean} | Attack: {mean < attack_threshold}')
+    return mean < attack_threshold
+
+def antispoofing_processing(image):
+    if is_attack(image):
+        print('Attack')
+        return np.zeros(image.shape)
+    else:
+        print('Real')
+        return image[:, :, 2]
+
+if __name__ == "__main__":
+    # Load image instead of using the camera
+    # image_path = "Imagens/detected_faces/grande_filtro/2024-06-09_12-38-54.png" # Real
+    # image_path = "Imagens/detected_faces/falsos/falso_papel_com_filtro-IPHONE/2024-07-07_16-49-08.png" #Falso papel
+    image_path = "Imagens/detected_faces/falsos/foto_tela/2024-07-07_23-28-21.png" #Falso tela
+
+    image = cv2.imread(image_path)
+
+    if image is not None:
+        image = antispoofing_processing(image)
+
+        x_start = int(image.shape[0]/2 - image.shape[0]/6)
+        x_end = int(image.shape[0]/2 + image.shape[0]/6)
+        y_start = int(image.shape[1]/2 - image.shape[1]/6)
+        y_end = int(image.shape[1]/2 + image.shape[1]/6)
+        rotated = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+        rotated = cv2.flip(rotated, 1)
+        cv2.imshow("Frame", rotated)
+        key = cv2.waitKey(0) & 0xFF
+
+        if key == ord("q"):
+            cv2.destroyAllWindows()
+    else:
+        print(f"Erro ao carregar a imagem: {image_path}")
--- capturer.py
+++ capturer.py
@@ -1,31 +1,58 @@
+
+
 # import module
 from datetime import datetime
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
-# Saved frame count
-count = 0
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
-# allow the camera to warmup
-time.sleep(0.1)
-# grab an image from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    image = frame.array
+import os
 
-    # show the frame
-    cv2.imshow("Frame", image)
+count = 0
+
+face_detector = cv2.CascadeClassifier("/home/bezerril/antispoofing/haarcascade_frontalface_default.xml")
+
+cv2.startWindowThread()
+
+camera = PiCamera()
+camera.resolution = (1280, 720)
+raw_capture = PiRGBArray(camera, size=(1280, 720))
+
+# Create a directory to store detected faces
+output_directory = "detected_faces"
+os.makedirs(output_directory, exist_ok=True)
+
+time.sleep(0.1)  # Allow the camera to warm up
+
+for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+    im = frame.array
+
+    grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(grey, 1.1, 5)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0))
+
+        # Generate a unique filename using timestamp for every saved image
+        timestamp = int(time.time())
+        filename = os.path.join(output_directory, f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.png')
+        cv2.imwrite(filename, im[y:y+h, x:x+w])  # Save only the detected face portion
+
+    cv2.imshow("Camera", im)
     key = cv2.waitKey(1) & 0xFF
 
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
+    # Clear the stream in preparation for the next frame
+    raw_capture.truncate(0)
 
-    # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
-    elif key == ord("s"):
-        cv2.imwrite(f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.png', image)
+    elif key == ord("c"):
+        filename = os.path.join(output_directory, f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.png')
+        cv2.imwrite(filename, im)
         print(f"Imagem salva: opencv{str(count)}.png")
         count += 1
+
+cv2.destroyAllWindows()
+    
+
+    
